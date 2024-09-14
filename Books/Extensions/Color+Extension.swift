9fd6bee30a7296109f2c907e8a1eedbf48e6912f
Stewart Lynch <slynch@createchsol.com>
2024-09-14 09:37:30 -0700

    Updated All Headers

--- Books/Extensions/Color+Extension.swift
+++ Books/Extensions/Color+Extension.swift
@@ -1,13 +1,16 @@
 //
-// Created for MyBooks
-// by  Stewart Lynch on 2023-10-15
+//----------------------------------------------
+// Original project:
+// by  Stewart Lynch on 9/14/24
 //
 // Follow me on Mastodon: @StewartLynch@iosdev.space
 // Follow me on Threads: @StewartLynch (https://www.threads.net)
 // Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
 // Subscribe on YouTube: https://youTube.com/@StewartLynch
 // Buy me a ko-fi:  https://ko-fi.com/StewartLynch
-
+//----------------------------------------------
+// Copyright © 2024 CreaTECH Solutions. All rights reserved.
 
 import SwiftUI
 
--- Books/Models/Author.swift
+++ Books/Models/Author.swift
@@ -1,9 +1,16 @@
 //
-//  Author.swift
-//  Books
+//----------------------------------------------
+// Original project:
+// by  Stewart Lynch on 9/14/24
 //
-//  Created by Stewart Lynch on 9/8/24.
-//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+//----------------------------------------------
+// Copyright © 2024 CreaTECH Solutions. All rights reserved.
 
 import SwiftUI
 import SwiftData
